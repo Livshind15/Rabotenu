957e524afde343a965a13710deb47c534b84efc4
daniel livshin <livshind12@gmail.com>
2020-07-16 23:41:17 +0300

    add delay for splace screen

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import Home from './containers/home/home';
 import Splash from './containers/splash/splash';
 import Explore from './containers/explore/explore';
 import Logo from './component/logo/logo';
+import ExploreResultView from './containers/explore/exploreResultView';
 const Stack = createStackNavigator();
 
 
@@ -16,13 +17,23 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     'OpenSansHebrew': require('./assets/fonts/OpenSansHebrew-Regular.ttf'),
   });
+  const [isDelay, setDelay] = React.useState(false)
 
-  const isInitialized = () => fontsLoaded;
+  const [isInitialized, setInitialized] = React.useState(false)
+  React.useEffect(() => {
+    if (fontsLoaded && isDelay) {
+      setInitialized(true);
+    }
+  }, [fontsLoaded,isDelay]);
+  setTimeout(() => {
+    setDelay(true)
+  }, 1000);
+
 
   return (
     <NavigationContainer>
 
-      {isInitialized() ? <Routes /> : <Splash />}
+      {isInitialized ? <Routes /> : <Splash />}
 
     </NavigationContainer>
 
@@ -31,9 +42,9 @@ export default function App() {
 
 const Routes = () => {
   return (
-    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerTintColor: '#00AABE', headerStyle: { backgroundColor: '#323232' } }}>
+    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerTintColor: '#00AABE' }}>
       <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
-      <Stack.Screen name="Explore" options={{ ...screenOptions, title: 'עיון' }} component={Explore} />
+      <Stack.Screen name="Explore" options={{ ...screenOptions, title: 'עיון' }} component={ExploreResultView} />
       <Stack.Screen name="Search" component={Home} />
       <Stack.Screen name="Acronym" component={Home} />
     </Stack.Navigator>
@@ -58,6 +69,12 @@ const styles = StyleSheet.create({
 const screenOptions = {
   headerTitleStyle: styles.headerTitle,
   headerLeft: ({ onPress }) => <Logo options={{ marginLeft: 12 }} onPress={onPress} />,
-  headerRight: () => (<></>)
+  headerRight: () => (<></>),
+  headerStyle: {
+    elevation: 0,
+    shadowOpacity: 0,
+    borderBottomWidth: 0,
+    backgroundColor: '#323232'
+  }
 }
 
