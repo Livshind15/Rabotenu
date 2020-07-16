b2364984a3e1afad3a96b6e95c3205755a00e9bc
daniel livshin <livshind12@gmail.com>
2020-07-16 23:42:25 +0300

    input component

---
+++ component/input/input.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { StyleSheet, View, TextInput, Platform } from 'react-native';
+
+export default function Input() {
+    return (
+        <View style={styles.MainContainer}>
+            <TextInput
+                placeholder="חיפוש חופשי"
+                placeholderTextColor='#CBCBCB'
+                underlineColorAndroid='transparent'
+                selectionColor={'#CBCBCB'}
+                underlineColorAndroid={'#FFFFFF00'}
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.TextInputStyleClass} />
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    MainContainer: {
+        width: '100%'
+    },
+    TextInputStyleClass: {
+        ...{
+            color: '#CBCBCB',
+            fontFamily: "OpenSansHebrew",
+            fontSize: 20,
+            textAlign: 'right',
+            height: 50,
+            paddingHorizontal: 40,
+            direction: 'rtl',
+            borderRadius: 50,
+            backgroundColor: "#FFFFFF",
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 1,
+            },
+            shadowOpacity: 0.18,
+            shadowRadius: 1.00,
+            elevation: 1,
+        }, ...(Platform.OS === 'web' ? {
+            outlineWidth: 0,
+            outlineColor: "transparent",
+        } : {})
+    }
+});
