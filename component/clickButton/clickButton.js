78de7688795fd4e59a17a538902ddb03454bec65
daniel livshin <livshind12@gmail.com>
2020-07-17 11:46:09 +0300

    change style for clickButton to support outline mode

--- component/clickButton/clickButton.js
+++ component/clickButton/clickButton.js
@@ -1,37 +1,44 @@
 import * as React from 'react';
-import { StyleSheet, TouchableOpacity,View,Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 
-export default function ClickButton() {
+export default function ClickButton({ optionsButton, optionsText, outline }) {
+    const styles = StyleSheet.create({
+        container: {
+            width: "100%",
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        button: {
+            ...{
+                width: '100%',
+                paddingVertical: 5,
+                borderRadius: 100
+            }, ...(outline ? {
+                borderColor:'#00AABE',
+                borderWidth:1
+            } : {
+                backgroundColor: '#00AABE',
+            })
+        },
+        text: {...{
+            fontFamily: "OpenSansHebrew",
+            textAlign: 'center',
+            fontSize: 24,
+        }, ...(outline ? {
+            color:'#00AABE',
+        } : {
+            color: '#fff',
+        })},
+    });
+
     return (
         <View style={styles.container}>
             <TouchableOpacity
-                style={styles.button}
+                style={[styles.button, optionsButton || []]}
                 underlayColor="#ffffff00" >
-                <Text style={styles.text}>חיפוש</Text>
+                <Text style={[styles.text, optionsText || []]}>חיפוש</Text>
             </TouchableOpacity>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-        margin: 3,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    button: {
-        width:'28%',
-
-        paddingVertical: 5,
-        backgroundColor:'#00AABE',
-        borderRadius:100
-
-    },
-    text: {
-        color: '#fff',
-        fontFamily: "OpenSansHebrew",
-        textAlign: 'center',
-        fontSize: 24,
-    },
-});
