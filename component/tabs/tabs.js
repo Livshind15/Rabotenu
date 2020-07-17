fdfebcf5d22348d3767898468502732dd4d1e295
daniel livshin <livshind12@gmail.com>
2020-07-17 11:45:41 +0300

    tabs continer

--- component/tabs/tabs.js
+++ component/tabs/tabs.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-import TabButton from '../tabButton/tabButton';
+import { StyleSheet, View } from 'react-native';
 
-export default function Tabs() {
+export default function Tabs({ children }) {
     const [tabIndex, setTabIndex] = React.useState(0)
     return (
         <View style={{ flex: 1, width: '100%', flexDirection: 'row' }}>
-            <TabButton isSelected={tabIndex === 0} onPress={()=>setTabIndex(0)} text={'חיפוש מראה מקום'}></TabButton>
-            <TabButton isSelected={tabIndex === 1}  onPress={()=>setTabIndex(1)} text={'תצוגת עץ'}></TabButton>
+            {React.Children.map(children, (child, index) => (
+                React.cloneElement(child, {
+                    key: index,
+                    isSelected: tabIndex === index,
+                    onPress: () => setTabIndex(index)
+                })
+            ))}
         </View>
     );
 }
