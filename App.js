bc8614c3cd4849e8a07f18a208884465ff1887c4
daniel livshin <livshind12@gmail.com>
2020-07-17 11:42:28 +0300

    add kitten ui provider

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import * as React from 'react';
+import * as eva from '@eva-design/eva';
 import 'react-native-gesture-handler';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useFonts } from '@use-expo/font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ApplicationProvider } from '@ui-kitten/components';
+
 
 import Home from './containers/home/home';
 import Splash from './containers/splash/splash';
 import Explore from './containers/explore/explore';
 import Logo from './component/logo/logo';
-import ExploreResultView from './containers/explore/exploreResultView';
 const Stack = createStackNavigator();
 
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     'OpenSansHebrew': require('./assets/fonts/OpenSansHebrew-Regular.ttf'),
+    'OpenSansHebrewBold': require('./assets/fonts/OpenSansHebrew-Bold.ttf'),
   });
   const [isDelay, setDelay] = React.useState(false)
 
@@ -24,18 +27,18 @@ export default function App() {
     if (fontsLoaded && isDelay) {
       setInitialized(true);
     }
-  }, [fontsLoaded,isDelay]);
+  }, [fontsLoaded, isDelay]);
   setTimeout(() => {
     setDelay(true)
   }, 1000);
 
 
   return (
-    <NavigationContainer>
-
-      {isInitialized ? <Routes /> : <Splash />}
-
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        {isInitialized ? <Routes /> : <Splash />}
+      </NavigationContainer>
+    </ApplicationProvider>
 
   );
 }
@@ -44,7 +47,7 @@ const Routes = () => {
   return (
     <Stack.Navigator initialRouteName="Home" screenOptions={{ headerTintColor: '#00AABE' }}>
       <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
-      <Stack.Screen name="Explore" options={{ ...screenOptions, title: 'עיון' }} component={ExploreResultView} />
+      <Stack.Screen name="Explore" options={{ ...screenOptions, title: 'עיון' }} component={Explore} />
       <Stack.Screen name="Search" component={Home} />
       <Stack.Screen name="Acronym" component={Home} />
     </Stack.Navigator>
