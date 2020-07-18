5990f5b25a26435195ee745d04007bed5727f79c
daniel livshin <livshind12@gmail.com>
2020-07-18 18:53:19 +0300

    exolore result view screens

--- containers/explore/exploreResultView.js
+++ containers/explore/exploreResultView.js
@@ -1,54 +1,196 @@
 import * as React from 'react';
 import { StyleSheet, View, StatusBar, Text, TouchableOpacity, Platform } from 'react-native';
+import { CheckBox } from '@ui-kitten/components';
+
 import Background from '../../component/background/background';
-import Tabs from '../../component/tabs/tabs';
 import Input from '../../component/input/input'
 import ClickButton from '../../component/clickButton/clickButton';
-export default function ExploreResultView() {
-  StatusBar.setBarStyle('light-content', true);
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function ExploreResultView({route }) {
+  const { result , searchInput} = route.params;
+  const [input, setInput] = React.useState(searchInput);
+  const [exploreResult, setExploreResult] = React.useState(attachKeyToArray(result, 'isCheck', false));
+
+  const setResultCheck = (index) => {
+    setExploreResult(exploreResult.map(result => {
+      if (result.key === index) {
+        return { ...result, isCheck: !result.isCheck }
+      }
+      return { ...result }
+    }))
+  }
+
+  const isResultSelect = () => exploreResult.some(result => result.isCheck)
+  
+  const countResultSelect = () => exploreResult.filter(result => result.isCheck).length
+
   return (
     <Background>
-      <View style={styles.tabs}>
-        <Tabs></Tabs>
-      </View>
       <View style={styles.page}>
         <View style={styles.input}>
           <View style={styles.buttonWrapper}>
-            <ClickButton
-              outline={true}
-              optionsButton={{
-                paddingVertical: 7
-              }}
-              optionsText={{
-                fontSize: 16,
-              }}
-            />
+            <ClickButton outline={true} optionsButton={{ paddingVertical: 8 }} optionsText={{ fontSize: 16 }}>חיפוש</ClickButton>
           </View>
           <View style={styles.inputWrapper}>
-            <Input options={{
-              fontSize: 16,
-              paddingHorizontal:20,
-              height: 40,
-            }} />
+            <Input value={input} onChange={setInput} options={{ fontSize: 16, paddingHorizontal: 20, height: 40 }} />
           </View>
         </View>
-        <View style={styles.button}>
+        <View style={styles.resultCountWrapper}>
+          <Text style={styles.titleResult}>תוצאות חיפוש</Text>
+          <Text style={styles.titleCount}>{`(סה"כ ${result.length})`}</Text>
+        </View>
+        <View style={styles.resultWrapper}>
+          <ScrollView>
+            {exploreResult.map((result, index) => <View key={index} style={styles.result}>
+              <View style={styles.checkBoxWrapper}>
+                <CheckBox
+                  checked={result.isCheck}
+                  onChange={() => { setResultCheck(result.key) }}>
+                </CheckBox>
+              </View>
+              <View style={styles.resultTitleWrapper}>
+                <TouchableOpacity
+                  underlayColor="#ffffff00"
+                  onPress={() => { setResultCheck(result.key) }}>
+                  <Text style={styles.resultTitle}>{result.title}</Text>
+                </TouchableOpacity>
+              </View>
+
+            </View>)}
+          </ScrollView>
+        </View>
+        <View style={styles.selectContainer}>
+          <TouchableOpacity
+            disabled={!isResultSelect()}
+            style={[styles.selectButton, isResultSelect() ? {} : styles.selectButtonDisable]}
+            underlayColor="#ffffff00" >
+            <Text style={[styles.selectButtonText, isResultSelect() ? {} : styles.selectButtonDisableText]} >
+
+              {isResultSelect() ?  `פתח ספרים נבחרים (${countResultSelect()})`:`פתח ספרים נבחרים`}
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </Background>
   );
 }
 
+const attachKeyToArray = (array, key, initValue) => {
+  return array.map(element => {
+    return {
+      ...element,
+      [key]: initValue
+    }
+  })
+}
+
 
 const styles = StyleSheet.create({
-  inputWrapper: {
-    flex: 0.8,
-    height: '100%',
-    justifyContent: 'center'
+  resultTitleWrapper: {
+    justifyContent: 'center',
+    flex: 1,
 
   },
+  resultTitle: {
+    fontFamily: "OpenSansHebrew",
+    textAlign: 'right',
+    justifyContent: 'center',
+    color: '#606060',
+    fontSize: 16,
+  },
+  checkBoxWrapper: {
+    alignItems: 'center',
+    width: 35
+  },
+  result: {
+    ...{
+      width: '100%',
+      height: 50,
+      paddingHorizontal: 8,
+      flexDirection: 'row-reverse',
+      borderWidth: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderColor: '#E4E4E4'
+    }
+    , ...(Platform.OS === 'web' ? {
+      flexDirection: 'row',
+    } : {})
+  },
+  selectButton: {
+    width: '95%',
+    height: '80%',
+    paddingVertical: 5,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00AABE',
+  },
+  selectButtonDisable: {
+    backgroundColor: '#D8D8D8',
+  },
+  selectButtonDisableText: {
+    fontFamily: "OpenSansHebrewBold",
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#A9A9A9'
+  },
+  selectContainer: {
+    width: '100%',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  selectButtonText: {
+    fontFamily: "OpenSansHebrewBold",
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#ECECEC'
+  },
+  resultWrapper: {
+    flex: 1,
+    width: '100%',
+    direction: 'rtl',
+
+  },
+  resultCountWrapper: {
+    height: 40,
+    width: '100%',
+    paddingHorizontal: 15,
+    backgroundColor: '#CBD4D3',
+    shadowColor: "#000",
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  titleResult: {
+    fontFamily: "OpenSansHebrewBold",
+    textAlign: 'center',
+    fontSize: 16,
+    color: "#6D6D6D"
+  },
+  titleCount: {
+    fontFamily: "OpenSansHebrew",
+    textAlign: 'center',
+    fontSize: 14,
+    color: '#878A8A'
+  },
+
+  inputWrapper: {
+    flex: 0.82,
+    height: '100%',
+    justifyContent: 'center'
+  },
   buttonWrapper: {
-    flex: 0.2,
+    flex: 0.18,
     height: '100%',
     paddingHorizontal: 10,
     justifyContent: 'center',
@@ -63,12 +205,12 @@ const styles = StyleSheet.create({
     zIndex: 5,
   },
   input: {
-    flex: 0.15,
-    width: '100%',
-    height: '100%',
+    width: '95%',
+    height: 55,
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
     paddingTop: 15,
@@ -82,8 +224,9 @@ const styles = StyleSheet.create({
   },
   page: {
     flex: 1,
-    width: '95%',
-    justifyContent: 'center',
-    alignContent: 'center'
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    alignContent: 'center',
   }
 });
---
+++ containers/explore/exploreTreeView.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Icon from "react-native-vector-icons/Entypo";
+import OctIcons from "react-native-vector-icons/Octicons";
+
+
+import Background from '../../component/background/background';
+import Accordian from '../../component/accordian/accordian';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import treeMock from './tree.mock';
+
+
+
+
+const ExploreTreeView = ({ navigation }) => {
+
+    return (
+        <Background>
+            <View style={styles.page}>
+                <ScrollView style={styles.scroll}>
+                    {treeMock.map((val, index) => <Accordian key={index} index={index} header={val.title} icon={<Icon name={'folder'} size={22} color={'#515151'} />}>
+                        <View style={styles.innerScroll}>
+                            {val.options.map((result, index) => <TouchableOpacity  underlayColor="#ffffff00" key={index} style={styles.resultContainer}>
+                                <Text style={styles.resultText}>{result.title}</Text>
+                                <OctIcons name={'book'} size={22} color={'#9AD3CE'}></OctIcons>
+                            </TouchableOpacity>)}
+                        </View>
+                    </Accordian>)}
+                </ScrollView>
+            </View>
+
+        </Background>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    resultText: {
+        fontSize: 16,
+        paddingRight: 15,
+        fontFamily: "OpenSansHebrew",
+        color: '#8D8C8C',
+    },
+    scroll: {
+        flex: 1,
+        width: '100%'
+    },
+    innerScroll: {
+        flex: 1,
+        width: '100%'
+    },
+    resultContainer: {
+        height: 40,
+        paddingLeft: 25,
+        paddingRight: 60,
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        borderColor: '#E4E4E4'
+    },
+    page: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'flex-start',
+        paddingTop: 4,
+        alignContent: 'center'
+    }
+});
+
+export default ExploreTreeView;
