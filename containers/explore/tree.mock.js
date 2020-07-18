860efa9d3bfae0ac5ff2412958f70c7d508a4949
daniel livshin <livshind12@gmail.com>
2020-07-18 18:50:36 +0300

    data mocks

---
+++ containers/explore/tree.mock.js
@@ -0,0 +1,28 @@
+export default [
+    {title:`תנ"ך ומפרשיו`,options:[
+        {title:'שאלה 1'},
+        {title:'שאלה 2'},
+        {title:'שאלה 3'},
+    ]},
+    {title:`חז"ל`,options:[
+        {title:'שאלה 1'},
+        {title:'שאלה 2'},
+        {title:'שאלה 3'},
+    ]},
+    {title:'הלכה ומנהג',options:[
+        {title:'שאלה 1'},
+        {title:'שאלה 2'},
+        {title:'שאלה 3'},
+    ]},
+    {title:`שאלות ותשובות`,options:[
+        {title:'שאלה 1'},
+        {title:'שאלה 2'},
+        {title:'שאלה 3'},
+    ]},
+    {title:'מחשבה ומוסר',options:[
+        {title:'שאלה 1'},
+        {title:'שאלה 2'},
+        {title:'שאלה 3'},
+    ]},
+
+]
