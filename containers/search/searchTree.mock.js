3810d4cb941f1e5fc44df362c3d8bdfc2863d9e6
daniel livshin <livshind12@gmail.com>
2020-07-20 19:53:13 +0300

    add data mock

---
+++ containers/search/searchTree.mock.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        title: `תנ"ך ומפרשיו`,
+        len: 666,
+        tree: [
+            {
+                title: 'מפרשים',
+                books: [
+                    { title: 'בראשית' },
+                    { title: 'ירמיה' },
+                ],
+                tree: [{
+                    title: 'רש"י',
+                    books: [
+                        { title: 'בראשית' },
+                        { title: 'ירמיה' },
+                    ],
+                    tree: [],
+                    len: 664
+                }],
+                len: 664
+            }
+        ],
+        books: [
+            { title: 'בראשית' },
+            { title: 'ירמיה' },
+        ]
+    }
+
+]
