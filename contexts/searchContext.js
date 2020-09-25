8ee96a4a15068461b6be7d69a1c8d8c6b6ef71dc
Daniel Livshin <livshind12@gmail.com>
2020-09-25 21:26:04 +0300

    update sdk

--- contexts/searchContext.js
+++ contexts/searchContext.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { flatten, isEqual, difference } from 'lodash';
 import { getBooksInGroup, addCheckForResources } from '../containers/resources/resources';
 
